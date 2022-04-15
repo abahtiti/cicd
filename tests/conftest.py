7a29494a788260b6bfb427c09a13fbe4d0105c5e
abahtiti <31974714+abahtiti@users.noreply.github.com>
2022-04-15 22:24:16 +1000

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ def bf_init():
 
     logging.getLogger("pybatfish").setLevel(logging.WARN)
 
-    bf = Session(host="localhost")
+    bf = Session(host="batfish")
     bf.init_snapshot(BF_SNAPSHOT_PATH, overwrite=True)
 
     return bf
